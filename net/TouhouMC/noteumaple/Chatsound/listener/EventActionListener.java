10450a902fcf31ac7127aed24a4268f4fffe53c3
BuildTools <crownpeace20@gmail.com>
2016-03-02 21:17:29 +0900

    user is good
    only one issue
    group keys is invalid

--- net/TouhouMC/noteumaple/Chatsound/listener/EventActionListener.java
+++ net/TouhouMC/noteumaple/Chatsound/listener/EventActionListener.java
@@ -1,7 +1,10 @@
 package net.TouhouMC.noteumaple.Chatsound.listener;
 
 import java.io.File;
+
 import net.TouhouMC.noteumaple.Chatsound.Chatsound;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -27,31 +30,36 @@ implements Listener {
     @EventHandler
     public void soundChat(AsyncPlayerChatEvent e) {
         Player pl = e.getPlayer();
+        String soundfullname;
+		String soundcutname;
         if (!pl.hasPermission("chatsound.speaker")) return;
-        if (conf.getString("user." + pl.getUniqueId() + ".sound") != null && conf.getString("user." + pl.getUniqueId() + ".sound") != "none") {
-            String soundcutname = conf.getString("user." + pl.getUniqueId() + ".sound");
-            String soundfullname = conf.getString("sounds." + soundcutname);
+		int volume;
+		int pitch;
+		if (conf.getInt("user." + pl.getUniqueId() + ".volume") != 0) {
+            soundcutname = conf.getString("user." + pl.getUniqueId() + ".sound");
+            soundfullname = conf.getString("sounds." + soundcutname);
             if (soundfullname == null) return;
-            int volume = conf.getInt("user." + pl.getUniqueId() + ".volume");
-            int pitch = conf.getInt("user." + pl.getUniqueId() + ".pitch");
+            volume = conf.getInt("user." + pl.getUniqueId() + ".volume");
+            pitch = conf.getInt("user." + pl.getUniqueId() + ".pitch");
             if (Sound.valueOf((String)soundfullname.toUpperCase()) == null) return;
-            for (Player listener : pl.getWorld().getPlayers()) {
-                if (!listener.hasPermission("chatsound.listen")) continue;
-                listener.playSound(listener.getLocation(), Sound.valueOf((String)soundfullname.toUpperCase()), (float)volume, (float)pitch);
+            for (Player lpl : Bukkit.getServer().getOnlinePlayers()) {
+                if (!lpl.hasPermission("chatsound.listen")) continue;
+                lpl.playSound(lpl.getLocation(), Sound.valueOf((String)soundfullname.toUpperCase()), (float)volume, (float)pitch);
             }
             return;
-        } else {
-            for (String group : conf.getConfigurationSection("group").getKeys(false)) {
-                String soundfullname;
-                if (conf.getString("group." + group + ".sound") == null || conf.getString("group." + group + ".sound") == "none" || !pl.hasPermission("chatsound.group." + group) || (soundfullname = conf.getString("sounds." + (conf.getString(new StringBuilder("group.").append(group).append(".sound").toString())))) == null) continue;
-                int volume = conf.getInt("group." + pl.getUniqueId() + ".volume");
-                int pitch = conf.getInt("group." + pl.getUniqueId() + ".pitch");
+        } 
+			for ( String group : conf.getConfigurationSection("group").getKeys(false)) {
+                if (conf.getString("group." + group.toLowerCase() + ".sound") == null || !pl.hasPermission("chatsound.group." + group.toLowerCase())) continue;
+                soundcutname = conf.getString("group." + group.toLowerCase() + ".sound");
+                soundfullname = conf.getString("sounds." + soundcutname);
+                if (soundfullname == null) continue;
+                volume = conf.getInt("group." + pl.getUniqueId() + ".volume");
+                pitch = conf.getInt("group." + pl.getUniqueId() + ".pitch");
                 if (Sound.valueOf((String)soundfullname.toUpperCase()) == null) continue;
-                for (Player listener : pl.getWorld().getPlayers()) {
-                    if (!listener.hasPermission("chatsound.listen")) continue;
-                    listener.playSound(listener.getLocation(), Sound.valueOf((String)soundfullname.toUpperCase()), (float)volume, (float)pitch);
+                for (Player lpl : Bukkit.getServer().getOnlinePlayers()) {
+                        if (!lpl.hasPermission("chatsound.listen")) continue;
+                        lpl.playSound(lpl.getLocation(), Sound.valueOf((String)soundfullname.toUpperCase()), (float)volume, (float)pitch);
                 }
             }
-        }
     }
 }
